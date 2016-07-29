231aac820f4fa9986e6536dfdaae699b1e7a4d40
dxoakes <donald.oakes@centurylink.com>
2016-07-29 10:30:28 -0600

    Removing Designer projects

--- com.centurylink.mdw.designer.ui/src/com/centurylink/mdw/plugin/server/FusePublishTask.java
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2014 CenturyLink, Inc. All Rights Reserved.
- */
-package com.centurylink.mdw.plugin.server;
-
-public class FusePublishTask extends ServiceMixPublishTask
-{
-
-}
