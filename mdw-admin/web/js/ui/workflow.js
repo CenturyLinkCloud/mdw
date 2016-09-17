3c4638d3e705c0bbc77681611f1401a8401a88e6
Donald Oakes <donald.oakes@centurylink.com>
2016-09-17 12:51:13 -0600

    Preliminary process def in AdminUI

--- mdw-admin/web/js/ui/workflow.js
+++ mdw-admin/web/js/ui/workflow.js
@@ -3,8 +3,9 @@
 
 var workflowMod = angular.module('mdwWorkflow', ['mdw']);
 
-workflowMod.controller('MdwWorkflowController', ['$scope', '$http', '$routeParams', 'mdw', 'util', 'Workflow',
-                                             function($scope, $http, $routeParams, mdw, util, Workflow) {
+workflowMod.controller('MdwWorkflowController', 
+    ['$scope', '$http', '$routeParams', 'mdw', 'util', 'Workflow', 'Diagram',
+    function($scope, $http, $routeParams, mdw, util, Workflow, Diagram) {
   
   $scope.init = function(canvas) {
     $scope.canvas = canvas;
@@ -16,15 +17,12 @@ workflowMod.controller('MdwWorkflowController', ['$scope', '$http', '$routeParam
     $scope.process = Workflow.retrieve({pkg: packageName, process: $scope.process.name}, function() {
       $scope.process.packageName = packageName;  // not returned in JSON
 
-      // draw something
-      var c = $scope.canvas[0];
-      var ctx = c.getContext("2d");
-      ctx.moveTo(0,0);
-      ctx.lineTo(200,100);
-      ctx.stroke();      
-      
+      $scope.diagram = new Diagram($scope.canvas[0], $scope.process);
+      $scope.diagram.draw();
     });
   };
+  
+  $scope
   
 }]);
 
@@ -32,6 +30,25 @@ workflowMod.factory('Workflow', ['$resource', 'mdw', function($resource, mdw) {
   return $resource(mdw.roots.services + '/services/Workflow/:pkg/:process', mdw.serviceParams(), {
     retrieve: { method: 'GET', isArray: false }
   });
+}]);
+
+workflowMod.factory('Diagram', ['mdw', 'util', function(mdw, util) {
+  var Diagram = function(canvas, process, instance) {
+    this.canvas = canvas;
+    this.process = process;
+    this.instance = instance ? instance : null;
+  };
+  
+  Diagram.prototype.draw = function() {
+    // draw something
+    var ctx = this.canvas.getContext("2d");
+    ctx.moveTo(0,0);
+    ctx.lineTo(200,100);
+    ctx.stroke();      
+  };
+  
+  return Diagram;
+  
 }]);
 
 // packageName and name must be populated on model object
