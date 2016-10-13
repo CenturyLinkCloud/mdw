b45337b0d9190e5133ae3df4bd5e0c9e3a2ae9c0
dxoakes <donald.oakes@centurylink.com>
2016-10-13 12:18:54 -0600

    Removing Designer projects from open-source

--- com.centurylink.mdw.designer.ui/src/com/centurylink/mdw/plugin/designer/model/ElementChangeListener.java
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2014 CenturyLink, Inc. All Rights Reserved.
- */
-package com.centurylink.mdw.plugin.designer.model;
-
-public interface ElementChangeListener
-{
-  public void elementChanged(ElementChangeEvent ece);
-}
--- com.centurylink.mdw.designer.ui/src/com/centurylink/mdw/plugin/designer/properties/editor/ValueChangeListener.java
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2014 CenturyLink, Inc. All Rights Reserved.
- */
-package com.centurylink.mdw.plugin.designer.properties.editor;
-
-public interface ValueChangeListener
-{
-  public void propertyValueChanged(Object newValue);
-}
