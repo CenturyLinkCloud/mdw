2b357f517be66c55b65fe5363853faab897b6a4f
dxoakes <donald.oakes@centurylink.com>
2017-01-19 15:27:09 -0700

    Adapter endpoint stubbing.

--- mdw-common/src/com/centurylink/mdw/model/event/AdapterStubRequest.java
+++ mdw-common/src/com/centurylink/mdw/model/event/AdapterStubRequest.java
@@ -3,10 +3,13 @@
  */
 package com.centurylink.mdw.model.event;
 
+import java.util.Map;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.centurylink.mdw.common.service.Jsonable;
+import com.centurylink.mdw.util.JsonUtil;
 
 public class AdapterStubRequest implements Jsonable {
 
@@ -15,6 +18,18 @@ public class AdapterStubRequest implements Jsonable {
     private String masterRequestId;
     public String getMasterRequestId() { return masterRequestId; }
     public void setMasterRequestId(String masterRequestId) { this.masterRequestId = masterRequestId; }
+
+    private String url;
+    public String getUrl() { return url; }
+    public void setUrl(String endpoint) { this.url = endpoint; }
+
+    private String method;
+    public String getMethod() { return method; }
+    public void setMethod(String method) { this.method = method; }
+
+    private Map<String,String> headers;
+    public Map<String,String> getHeaders() { return headers; }
+    public void setHeaders(Map<String,String> headers) { this.headers = headers; }
 
     private String content;
     public String getContent() { return content; }
@@ -29,6 +44,12 @@ public class AdapterStubRequest implements Jsonable {
         JSONObject requestJson = json.getJSONObject(getJsonName());
         this.masterRequestId = requestJson.getString("masterRequestId");
         this.content = requestJson.getString("content");
+        if (requestJson.has("url"))
+            this.url = requestJson.getString("url");
+        if (requestJson.has("method"))
+            this.method = requestJson.getString("method");
+        if (requestJson.has("headers"))
+            this.headers = JsonUtil.getMap(requestJson.getJSONObject("headers"));
     }
 
     public JSONObject getJson() throws JSONException {
@@ -36,6 +57,12 @@ public class AdapterStubRequest implements Jsonable {
         JSONObject requestJson = new JSONObject();
         requestJson.put("masterRequestId", masterRequestId);
         requestJson.put("content", content);
+        if (url != null)
+            requestJson.put("url", url);
+        if (method != null)
+            requestJson.put("method", method);
+        if (headers != null)
+            requestJson.put("headers", JsonUtil.getJson(headers));
         json.put(getJsonName(), requestJson);
         return json;
     }
