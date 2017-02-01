28dfea67f528f12ed8ea508dcf47b976b28cf96b
Jack Brojde <Jack.Brojde@centurylink.com>
2017-02-01 16:47:25 -0700

    Add watching/WebWatching capability to all Loggers, not just SimpleLogger

--- mdw-common/src/com/centurylink/mdw/util/log/AbstractStandardLoggerBase.java
+++ mdw-common/src/com/centurylink/mdw/util/log/AbstractStandardLoggerBase.java
@@ -5,6 +5,16 @@ package com.centurylink.mdw.util.log;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.centurylink.mdw.common.service.MdwWebSocketServer;
+import com.centurylink.mdw.config.PropertyManager;
+import com.centurylink.mdw.constant.PropertyNames;
+import com.centurylink.mdw.soccom.SoccomClient;
 
 public abstract class AbstractStandardLoggerBase implements StandardLogger {
 
@@ -12,6 +22,12 @@ public abstract class AbstractStandardLoggerBase implements StandardLogger {
     public static final String DEFAULT_PORT = "7181";
 
     private static final String SENTRY_MARK = "[SENTRY-MARK] ";
+
+    private static final String MESSAGE_REG_EX = "\\[\\(.\\)([0-9.:]+) p([0-9]+)\\.([0-9]+) ([a-z])([^\\]]+)\\] (.*)";
+
+    private static Pattern pattern = Pattern.compile(MESSAGE_REG_EX, Pattern.DOTALL);
+
+    protected String watcher = null;
 
     public String getDefaultHost() {
         return DEFAULT_HOST;
@@ -23,6 +39,10 @@ public abstract class AbstractStandardLoggerBase implements StandardLogger {
 
     public String getSentryMark() {
         return SENTRY_MARK;
+    }
+
+    public void refreshCache() {
+        watcher = PropertyManager.getProperty(PropertyNames.MDW_LOGGING_WATCHER);
     }
 
     protected String generate_log_line(char type, String tag, String message) {
@@ -42,6 +62,65 @@ public abstract class AbstractStandardLoggerBase implements StandardLogger {
         sb.append("] ");
         sb.append(message);
         return sb.toString();
+    }
+
+    public boolean watching() {
+        return watcher != null;
+    }
+
+    protected JSONObject buildJSONLogMessage(String message) throws JSONException {
+        JSONObject obj = new JSONObject();
+        obj.put("name", "LogWatcher");
+
+        Matcher matcher = pattern.matcher(message);
+
+        if (matcher.matches())
+        {
+            obj.put("time", matcher.group(1));
+            obj.put("procId", new Long(matcher.group(2)));
+            obj.put("procInstId", new Long(matcher.group(3)));
+            obj.put("subtype", matcher.group(4));
+            obj.put("id", matcher.group(5));
+            obj.put("msg", matcher.group(6));
+        }
+
+        return obj;
+    }
+
+    protected void sendToWatchers(String message) {
+        if (watching())  // Designer
+            sendToWatcher(message);
+
+        try {
+            JSONObject jsonObj = buildJSONLogMessage(message);
+
+            if (jsonObj != null && jsonObj.has("ProcInstId")) {
+                if (MdwWebSocketServer.getInstance().hasInterestedConnections(jsonObj.getString("ProcInstId")))
+                    sendToWebWatcher(jsonObj);
+            }
+        }
+        catch (Throwable e) {};
+    }
+
+    protected void sendToWatcher(String message) {
+        SoccomClient client = null;
+        try {
+            String[] spec = watcher.split(":");
+            String host = spec.length>0?spec[0]:getDefaultHost();
+            String port = spec.length>1?spec[1]:getDefaultPort();
+            client = new SoccomClient(host, port, null);
+            client.putreq(message);
+        } catch (Exception e) {
+            watcher = null;
+            System.out.println("Exception when sending log messages to watcher - turn it off");
+            e.printStackTrace();
+        } finally {
+            if (client!=null) client.close();
+        }
+    }
+
+    protected void sendToWebWatcher(JSONObject message) throws JSONException {
+        MdwWebSocketServer.getInstance().send(message.toString(2), message.getString("ProcInstId"));
     }
 
 }
