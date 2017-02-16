fa31ff3bb8caaa44e3f4f8b80404a439ef014d50
Manoj Agrawal <Manoj.Agrawal@Centurylink.com>
2017-02-16 14:49:37 -0700

    Updated Http Helper functionality to remove userVO use and timout setting. Used server side property for url.

--- mdw-hub/web/js/message.js
+++ mdw-hub/web/js/message.js
@@ -3,18 +3,31 @@
 
 var messageMod = angular.module('message', ['ngResource', 'ui.bootstrap', 'mdw']);
 
-messageMod.controller('MessageController', ['$scope', '$location', '$http', 'mdw', 'Message', 'HTTP_METHODS',
-  											function($scope, $location, $http, mdw, Message, HTTP_METHODS) {
+messageMod.controller('MessageController', ['$scope', '$location', '$http', 'mdw', 'util', 'Message', 'HTTP_METHODS',
+  											function($scope, $location, $http, mdw, util, Message, HTTP_METHODS) {
 
   $scope.waitingForResponse = false;
   $scope.httpHelper = {};
   $scope.httpHelper.httpMethods = [];
   $scope.httpHelper.httpMethod = 'POST';
   $scope.httpHelper.timeOut = 15000;
-  $scope.httpHelper.url = mdw.roots.services + "/Services/REST";
+  
+  $scope.httpHelper.url = "http://localhost:8080/mdw/services";
   $scope.httpHelper.responseCode = "";
   $scope.httpHelper.httpMethods = HTTP_METHODS.slice();
-  
+
+  var mdwProps = util.getMdwProperties();
+  if (mdwProps) {
+    $scope.httpHelper.url = mdwProps["mdw.services.url"] + "/services";
+  }
+  else {
+    util.loadMdwProperties().then(function(response) {
+      mdwProps = util.getMdwProperties();
+      $scope.httpHelper.url = mdwProps["mdw.services.url"] + "/services";
+    });
+  }
+
+ 
   $scope.getMethod = function(selFieldValue) {
     if ($scope.httpHelper.httpMethods) {
       for (var i = 0; i < $scope.httpHelper.httpMethods.length; i++) {
--- mdw-services/src/com/centurylink/mdw/service/rest/HttpMessages.java
+++ mdw-services/src/com/centurylink/mdw/service/rest/HttpMessages.java
@@ -15,12 +15,9 @@ import org.json.JSONObject;
 
 import com.centurylink.mdw.common.service.Query;
 import com.centurylink.mdw.common.service.ServiceException;
-import com.centurylink.mdw.model.listener.Listener;
 import com.centurylink.mdw.model.message.HttpMessage;
 import com.centurylink.mdw.model.user.Role;
-import com.centurylink.mdw.model.user.User;
 import com.centurylink.mdw.model.user.UserAction.Entity;
-import com.centurylink.mdw.services.ServiceLocator;
 import com.centurylink.mdw.services.rest.JsonRestService;
 import com.centurylink.mdw.util.HttpHelper;
 import com.centurylink.mdw.util.StringHelper;
@@ -63,15 +60,8 @@ public class HttpMessages extends JsonRestService {
                 response = "Missing payload for HTTP POST method \nRequest: " + content;
             }
             else {
-                User userVO = ServiceLocator.getUserManager().getUser(requestMessage.getUser());
-                if (userVO == null)
-                    throw new ServiceException("User not found: " + requestMessage.getUser());
                 httpClient = new HttpHelper(new URL(requestMessage.getUrl()));
                 httpClient.setHeaders(headers);
-                if (requestMessage.getTimeout() == null || requestMessage.getTimeout() == 0)
-                {
-                    requestMessage.setTimeout(new Integer(15000));
-                }
                 response = httpClient.post(requestMessage.getRequestMessage(), requestMessage.getTimeout(), requestMessage.getTimeout());
                 code = httpClient.getResponseCode();
             }
@@ -109,15 +99,7 @@ public class HttpMessages extends JsonRestService {
 
         try {
             Query query = getQuery(path, headers);
-            String userCuid = headers.get(Listener.AUTHENTICATED_USER_HEADER);
-            User userVO = ServiceLocator.getUserManager().getUser(userCuid);
-            if (userVO == null)
-                throw new ServiceException("User not found: " + userCuid);
-            if (query.getFilter("timeOut") == null || Integer.parseInt(query.getFilter("timeOut")) == 0)
-            {
-                requestMessage.setTimeout(new Integer(15000));
-            }
-            else {
+            if (query.getFilter("timeOut") != null || Integer.parseInt(query.getFilter("timeOut")) != 0) {
                 requestMessage.setTimeout(Integer.parseInt(query.getFilter("timeOut")));
             }
             httpClient = new HttpHelper(new URL(query.getFilter("url")));
@@ -163,17 +145,10 @@ public class HttpMessages extends JsonRestService {
                 response = "Missing payload for HTTP PUT method \nRequest: " + content;
             }
             else {
-                User userVO = ServiceLocator.getUserManager().getUser(requestMessage.getUser());
-                if (userVO == null)
-                    throw new ServiceException("User not found: " + requestMessage.getUser());
                 httpClient = new HttpHelper(new URL(requestMessage.getUrl()));
                 httpClient.setHeaders(headers);
                 httpClient.setConnectTimeout(requestMessage.getTimeout());
                 httpClient.setReadTimeout(requestMessage.getTimeout());
-                if (requestMessage.getTimeout() == null || requestMessage.getTimeout() == 0)
-                {
-                    requestMessage.setTimeout(new Integer(15000));
-                }
 
                 response = httpClient.put(requestMessage.getRequestMessage());
                 code = httpClient.getResponseCode();
@@ -219,15 +194,7 @@ public class HttpMessages extends JsonRestService {
 
         try {
             Query query = getQuery(path, headers);
-            String userCuid = headers.get(Listener.AUTHENTICATED_USER_HEADER);
-            User userVO = ServiceLocator.getUserManager().getUser(userCuid);
-            if (userVO == null)
-                throw new ServiceException("User not found: " + userCuid);
-            if (query.getFilter("timeOut") == null || Integer.parseInt(query.getFilter("timeOut")) == 0)
-            {
-                requestMessage.setTimeout(new Integer(15000));
-            }
-            else {
+            if (query.getFilter("timeOut") != null || Integer.parseInt(query.getFilter("timeOut")) != 0) {
                 requestMessage.setTimeout(Integer.parseInt(query.getFilter("timeOut")));
             }
             httpClient = new HttpHelper(new URL(query.getFilter("url")));
@@ -273,17 +240,10 @@ public class HttpMessages extends JsonRestService {
                 response = "Missing payload for HTTP PATCH method \nRequest: " + content;
             }
             else {
-                User userVO = ServiceLocator.getUserManager().getUser(requestMessage.getUser());
-                if (userVO == null)
-                    throw new ServiceException("User not found: " + requestMessage.getUser());
                 httpClient = new HttpHelper(new URL(requestMessage.getUrl()));
                 httpClient.setHeaders(headers);
                 httpClient.setConnectTimeout(requestMessage.getTimeout());
                 httpClient.setReadTimeout(requestMessage.getTimeout());
-                if (requestMessage.getTimeout() == null || requestMessage.getTimeout() == 0)
-                {
-                    requestMessage.setTimeout(new Integer(15000));
-                }
 
                 response = httpClient.patch(requestMessage.getRequestMessage());
                 code = httpClient.getResponseCode();
