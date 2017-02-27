4fdf547f7118cfca1affcbdb897b00387fb68833
dxoakes <donald.oakes@centurylink.com>
2017-02-27 12:27:57 -0700

    Issue #103.

--- mdw-hub/src/com/centurylink/mdw/hub/servlet/TestCaseServlet.java
+++ mdw-hub/src/com/centurylink/mdw/hub/servlet/TestCaseServlet.java
@@ -8,13 +8,17 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.centurylink.mdw.app.ApplicationContext;
+import com.centurylink.mdw.model.listener.Listener;
 import com.centurylink.mdw.services.ServiceLocator;
 import com.centurylink.mdw.services.TestingServices;
+import com.centurylink.mdw.util.HttpHelper;
 
 /**
  * Serves up raw test cases and related resources.
@@ -25,29 +29,39 @@ public class TestCaseServlet extends AssetContentServlet {
     throws ServletException, IOException {
 
         if ("/testResult".equals(request.getServletPath())) {
-            TestingServices testingServices = ServiceLocator.getTestingServices();
-            File resultsDir = testingServices.getTestResultsDir();
+            if (ApplicationContext.isMasterServer() || request.getHeader(Listener.METAINFO_MASTER_OP) != null) {
 
-            InputStream in = null;
-            OutputStream out = response.getOutputStream();
-            try {
-                File file = new File(resultsDir + request.getPathInfo());
-                if (!file.isFile()) {
-                    response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-                    return;
+                TestingServices testingServices = ServiceLocator.getTestingServices();
+                File resultsDir = testingServices.getTestResultsDir();
+
+                InputStream in = null;
+                OutputStream out = response.getOutputStream();
+                try {
+                    File file = new File(resultsDir + request.getPathInfo());
+                    if (!file.isFile()) {
+                        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                        return;
+                    }
+                    response.setContentType("text/plain");
+                    in = new FileInputStream(file);
+                    int read = 0;
+                    byte[] bytes = new byte[1024];
+                    while ((read = in.read(bytes)) != -1)
+                        out.write(bytes, 0, read);
                 }
+                finally {
+                    if (in != null)
+                        in.close();
+                }
+            }
+            else {
+                String url = "http://" + ApplicationContext.getMasterServer() + "/"
+                        + ApplicationContext.getServicesContextRoot() + "/testResult" + request.getPathInfo();
+                HttpHelper httpHelper = new HttpHelper(new URL(url));
+                httpHelper.setHeader(Listener.METAINFO_MASTER_OP, "true");
                 response.setContentType("text/plain");
-                in = new FileInputStream(file);
-                int read = 0;
-                byte[] bytes = new byte[1024];
-                while ((read = in.read(bytes)) != -1)
-                    out.write(bytes, 0, read);
+                response.getWriter().println(httpHelper.get());
             }
-            finally {
-                if (in != null)
-                    in.close();
-            }
-
         }
         else {
             super.doGet(request, response);
