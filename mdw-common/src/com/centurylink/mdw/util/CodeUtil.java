3ab253065c4faca0cdd655818598848db9693d77
dxoakes <donald.oakes@centurylink.com>
2017-03-23 12:57:12 -0600

    Internal: Revised copyright headers.

--- mdw-common/src/com/centurylink/mdw/util/CodeUtil.java
+++ mdw-common/src/com/centurylink/mdw/util/CodeUtil.java
@@ -1,5 +1,17 @@
-/**
- * Copyright (c) 2016 CenturyLink, Inc. All Rights Reserved.
+/*
+ * Copyright (C) 2017 CenturyLink, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.centurylink.mdw.util;
 
@@ -10,7 +22,7 @@ import com.centurylink.mdw.util.file.FileHelper;
 
 public class CodeUtil {
 
-    public static final String HEADER_COMMENT = "/**\r\n * Copyright (c) 2016 CenturyLink, Inc. All Rights Reserved.\r\n */\r\n";
+    public static final String HEADER_COMMENT = "/*\r\n * Copyright (C) 2017 CenturyLink, Inc.\r\n *\r\n * Licensed under the Apache License, Version 2.0 (the \"License\");\r\n * you may not use this file except in compliance with the License.\r\n * You may obtain a copy of the License at\r\n *\r\n *      http://www.apache.org/licenses/LICENSE-2.0\r\n *\r\n * Unless required by applicable law or agreed to in writing, software\r\n * distributed under the License is distributed on an \"AS IS\" BASIS,\r\n * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\r\n * See the License for the specific language governing permissions and\r\n * limitations under the License.\r\n */\r\n";
 
     /**
      * @param args h c:/workspaces/mdw (for adding standard header comments to *.java)
