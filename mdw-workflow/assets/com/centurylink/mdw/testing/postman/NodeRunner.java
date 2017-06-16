a945824ab9b2fa31342c302f55d01f76cd2638cd
dxoakes <donald.oakes@centurylink.com>
2017-06-16 16:18:50 -0600

    Issue #125

--- mdw-workflow/assets/com/centurylink/mdw/testing/postman/NodeRunner.java
+++ mdw-workflow/assets/com/centurylink/mdw/testing/postman/NodeRunner.java
@@ -4,7 +4,7 @@
 package com.centurylink.mdw.testing.postman;
 
 import com.centurylink.mdw.common.service.ServiceException;
-import com.centurylink.mdw.model.asset.AssetInfo;
+import com.centurylink.mdw.services.AssetServices;
 import com.centurylink.mdw.services.ServiceLocator;
 import com.centurylink.mdw.test.TestCase;
 import com.centurylink.mdw.test.TestCaseItem;
@@ -17,13 +17,40 @@ import com.eclipsesource.v8.V8Object;
  * Runs NodeJS test assets (through runner.js).
  */
 public class NodeRunner {
+    static final String PARSER = "com.centurylink.mdw.testing.postman/parser.js";
     static final String RUNNER = "com.centurylink.mdw.testing.postman/runner.js";
 
     public void run(TestCase testCase) throws ServiceException {
-        // TODO honor testCase
 
-        AssetInfo asset = ServiceLocator.getAssetServices().getAsset(RUNNER);
-        final NodeJS nodeJS = NodeJS.createNodeJS();
+        AssetServices assets = ServiceLocator.getAssetServices();
+
+        NodeJS nodeJS = NodeJS.createNodeJS();
+
+        final Result parseResult = new Result();
+        JavaCallback callback = new JavaCallback() {
+            public Object invoke(V8Object receiver, V8Array parameters) {
+                V8Object resultObj = parameters.getObject(0);
+                parseResult.status = resultObj.getString("status");
+                parseResult.message = resultObj.getString("message");
+                System.out.println("  Parse Result: " + parseResult);
+                resultObj.release();
+                return null;
+            }
+        };
+        nodeJS.getRuntime().registerJavaMethod(callback, "setParseResult");
+
+        nodeJS.exec(assets.getAsset(PARSER).getFile());
+        while (nodeJS.isRunning()) {
+            nodeJS.handleMessage();
+        }
+
+        if (!parseResult.status.equals("OK"))
+            throw new ServiceException(PARSER + parseResult);
+
+        nodeJS.release();
+
+        nodeJS = NodeJS.createNodeJS();
+
         V8Object testObj = new V8Object(nodeJS.getRuntime()).add("coll", testCase.getAsset().getFile().getAbsolutePath());
         final V8Array testItems = new V8Array(nodeJS.getRuntime());
         for (TestCaseItem item : testCase.getItems()) {
@@ -34,31 +61,40 @@ public class NodeRunner {
         testObj.add("items", testItems);
         testItems.release();
 
-        JavaCallback callback = new JavaCallback() {
+        callback = new JavaCallback() {
             public Object invoke(V8Object receiver, V8Array parameters) {
               return testObj;
             }
         };
         nodeJS.getRuntime().registerJavaMethod(callback, "getTestCase");
 
+        final Result testResult = new Result();
         callback = new JavaCallback() {
             public Object invoke(V8Object receiver, V8Array parameters) {
                 V8Object resultObj =parameters.getObject(0);
-                System.out.println("  Test Result: " + resultObj.getString("status") + ": " + resultObj.getString("message"));
+                testResult.status = resultObj.getString("status");
+                testResult.message = resultObj.getString("message");
+                System.out.println("  Test Result: " + testResult);
                 resultObj.release();
                 return null;
             }
         };
-        nodeJS.getRuntime().registerJavaMethod(callback, "setResult");
+        nodeJS.getRuntime().registerJavaMethod(callback, "setTestResult");
 
-
-        nodeJS.exec(asset.getFile());
+        nodeJS.exec(assets.getAsset(RUNNER).getFile());
         while (nodeJS.isRunning()) {
             nodeJS.handleMessage();
         }
 
         testObj.release();
-
         nodeJS.release();
+    }
+
+    private class Result {
+        String status;
+        String message;
+        public String toString() {
+            return status + ": " + message;
+        }
     }
 }
--- mdw-workflow/assets/com/centurylink/mdw/testing/postman/runner.js
+++ mdw-workflow/assets/com/centurylink/mdw/testing/postman/runner.js
@@ -1,9 +1,45 @@
+'use strict'; 
 try {
   var testCase = getTestCase();
-  console.log('running postman test collection:\n  ' + JSON.stringify(testCase, null, 2));
-  setResult({ status: 'Passed', message: 'Test succeeded' });
+  console.log('running test case:\n  ' + JSON.stringify(testCase, null, 2));
+  
+  var fs = require('fs');
+  var postman = JSON.parse(fs.readFileSync(testCase.coll, 'utf8'));
+  
+  console.log('read postman test collection:\n ' + testCase.coll);
+  
+  var request = require('request');
+  
+  for (let i = 0; i < postman.item.length; i++) {
+    var postmanItem = postman.item[i];
+    var shouldRun = (testCase.items && testCase.items.find(function(item) {
+      console.log("NAME: " + item.name);
+      console.log("PI: " + postmanItem.name);
+      return item.name == postmanItem.name;
+    }));
+    
+    if (shouldRun) {
+      console.log('will run: ' + JSON.stringify(postmanItem, null, 2));
+    }
+  }
+  
+  // TODO set expected result YAML
+  request(postmanItem.request.url, function(error, response, body) {
+    if (error) {
+      console.log('error:', error);
+      setTestResult({ status: 'Errored', message: error.toString() });
+    }
+    else {
+      // TODO set actual result YAML
+      console.log('statusCode:', response && response.statusCode); 
+      console.log('body:', body); 
+      setTestResult({ status: 'Passed', message: 'Test succeeded' });
+    }
+  });
 }
 catch (err) {
   // if not caught, VM can System.exit()
   console.log(err);
+  setTestResult({ status: 'Errored', message: err.toString() });  
+  
 }
