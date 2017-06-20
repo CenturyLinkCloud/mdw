e334ad5a6f47f007fe31a7ad47e632db6692487e
dxoakes <donald.oakes@centurylink.com>
2017-06-20 10:48:24 -0600

    progress on issue #125

--- mdw-workflow/assets/com/centurylink/mdw/testing/postman/runner.js
+++ mdw-workflow/assets/com/centurylink/mdw/testing/postman/runner.js
@@ -6,35 +6,63 @@ try {
   var fs = require('fs');
   var postman = JSON.parse(fs.readFileSync(testCase.coll, 'utf8'));
   
+  var jsYaml = require('js-yaml');
+  
   console.log('read postman test collection:\n ' + testCase.coll);
   
   var request = require('request');
   
-  for (let i = 0; i < postman.item.length; i++) {
-    var postmanItem = postman.item[i];
-    var shouldRun = (testCase.items && testCase.items.find(function(item) {
-      console.log("NAME: " + item.name);
-      console.log("PI: " + postmanItem.name);
-      return item.name == postmanItem.name;
+  var postmanItems = [];
+  for (let i = 0; i < testCase.items.length; i++) {
+    
+    var pmItem = (testCase.items[i] && postman.item.find(function(item) {
+      return item.name == testCase.items[i].name;
     }));
     
-    if (shouldRun) {
-      console.log('will run: ' + JSON.stringify(postmanItem, null, 2));
+    if (pmItem) {
+      postmanItems.push(pmItem);
     }
   }
   
-  // TODO set expected result YAML
-  request(postmanItem.request.url, function(error, response, body) {
-    if (error) {
-      console.log('error:', error);
-      setTestResult({ status: 'Errored', message: error.toString() });
+  postmanItems.forEach(function(postmanItem) {
+    console.log('will run: ' + JSON.stringify(postmanItem, null, 2));
+    console.log('postmanItem.request.url: ' + postmanItem.request.url);
+    
+    // TODO substs in req 
+    var req = {
+      url: postmanItem.request.url,
+      method: postmanItem.request.method,
+    };
+    if (postmanItem.request.header) {
+      req.header = {};
+      for (let i = 0; i < postmanItem.request.header.length; i++) {
+        var postmanHeader = postmanItem.request.header[i];
+        req.header[postmanHeader.key] = postmanHeader.value;
+      }
     }
-    else {
-      // TODO set actual result YAML
-      console.log('statusCode:', response && response.statusCode); 
-      console.log('body:', body); 
-      setTestResult({ status: 'Passed', message: 'Test succeeded' });
+    if (postmanItem.request.body && postmanItem.request.body.raw) {
+      req.body = postmanItem.request.body.raw.replace(/\r/g, '');
     }
+
+    console.log('REQUEST JSON:\n' + JSON.stringify(req, null, 2));
+    
+    // save request yaml
+    var reqYaml = jsYaml.safeDump(req, {noCompatMode: true});
+    console.log('REQUEST YAML:\n' + reqYaml);
+    
+    // expected result (request and response) YAML
+    request(postmanItem.request.url, function(error, response, body) {
+      if (error) {
+        console.log('error:', error);
+        setTestResult({ status: 'Errored', message: error.toString() });
+      }
+      else {
+        // TODO set actual result (request and response) YAML
+        console.log('statusCode:', response && response.statusCode); 
+        console.log('body:', body); 
+        setTestResult({ status: 'Passed', message: 'Test succeeded' });
+      }
+    });
   });
 }
 catch (err) {
