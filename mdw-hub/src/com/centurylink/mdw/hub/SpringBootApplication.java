f74a9ed2db7cc9f43ef2d6f30b36c23d04cecf4c
dxoakes <donald.oakes@centurylink.com>
2017-06-20 15:50:38 -0600

    Build spring boot jar.

---
+++ mdw-hub/src/com/centurylink/mdw/hub/SpringBootApplication.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2017 CenturyLink, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.centurylink.mdw.hub;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@ServletComponentScan
+public class SpringBootApplication {
+
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(SpringBootApplication.class, args);
+        }
+        catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory embeddedServletContainerFactory(ApplicationContext ctx) {
+        // TODO parameterized port
+        return new TomcatEmbeddedServletContainerFactory(8080);
+    }
+
+}
