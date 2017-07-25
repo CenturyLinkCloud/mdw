521a90696720c8a6f9a7f9438850183e4ec28144
Manoj Agrawal <manoj.agrawal@centurylink.com>
2017-07-25 10:22:09 -0600

    AppFog compatible env variable name

--- mdw-common/src/com/centurylink/mdw/constant/PropertyNames.java
+++ mdw-common/src/com/centurylink/mdw/constant/PropertyNames.java
@@ -177,7 +177,7 @@ public class PropertyNames {
     public static final String MDW_GIT_BRANCH = "mdw.git.branch";
     public static final String MDW_GIT_TRUSTED_HOST = "mdw.git.trusted.host";
     public static final String MDW_GIT_AUTO_PULL = "mdw.git.auto.pull";
-    public static final String MDW_GITHUB_SECRET_TOKEN = "mdw.github.secret.token";
+    public static final String MDW_GITHUB_SECRET_TOKEN = "MDW_GITHUB_SECRET_TOKEN";
 
     // Task with these status will not be cancelled when process instance completes
     public static final String FINAL_TASK_STATUSES = "mdw.task.statuses.final";
