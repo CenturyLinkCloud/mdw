d29884fe1d5aaa8b5683753534fd6659b06d1831
dxoakes <donald.oakes@centurylink.com>
2017-08-03 12:49:22 -0600

    accessible hub ui

--- mdw-hub/web/js/ui/drawing-constants.js
+++ mdw-hub/web/js/ui/drawing-constants.js
@@ -2,26 +2,4 @@
 
 var dcMod = angular.module('drawingConstants', []);
 
-dcMod.constant('DC', {
-  DEFAULT_FONT: {
-    FONT: '12px sans-serif',
-    SIZE: 12,
-  },
-  TITLE_FONT: {
-    FONT: 'bold 18px sans-serif',
-    SIZE: 18
-  },
-  DEFAULT_LINE_WIDTH: 1,
-  DEFAULT_COLOR: 'black',
-  HYPERLINK_COLOR: '#1565c0',
-  LINE_COLOR: 'green',
-  META_COLOR: 'gray',
-  BOX_OUTLINE_COLOR: 'black',
-  TRANSPARENT: 'rgba(0, 0, 0, 0)',
-  BOX_ROUNDING_RADIUS: 12,
-  ANCHOR_W: 3,
-  ANCHOR_COLOR: '#ec407a',
-  ANCHOR_HIT_W: 8,
-  MIN_DRAG: 3
-  
-});
+dcMod.constant('DC', mdwUi.DC);
