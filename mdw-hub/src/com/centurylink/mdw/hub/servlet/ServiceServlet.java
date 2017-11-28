8018794f49350cc3943c4cbe474ca4a67bb941fa
dxoakes <donald.oakes@centurylink.com>
2017-11-28 12:09:56 -0700

    Avoid double response commit on REST auth failure.

--- mdw-hub/src/com/centurylink/mdw/hub/servlet/ServiceServlet.java
+++ mdw-hub/src/com/centurylink/mdw/hub/servlet/ServiceServlet.java
@@ -15,9 +15,7 @@
  */
 package com.centurylink.mdw.hub.servlet;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Enumeration;
@@ -31,7 +29,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.centurylink.mdw.app.ApplicationContext;
 import com.centurylink.mdw.common.service.ServiceException;
-import com.centurylink.mdw.listener.ListenerHelper;
+import com.centurylink.mdw.model.Status;
 import com.centurylink.mdw.model.listener.Listener;
 import com.centurylink.mdw.model.user.AuthenticatedUser;
 import com.centurylink.mdw.util.AuthUtils;
@@ -187,13 +185,8 @@ public abstract class ServiceServlet extends HttpServlet {
     }
 
     protected String createErrorResponseMessage(HttpServletRequest request,
-            Map<String, String> metaInfo, ServiceException ex) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
-        StringBuffer requestBuffer = new StringBuffer();
-        String line;
-        while ((line = reader.readLine()) != null)
-            requestBuffer.append(line).append('\n');
-        return new ListenerHelper().createErrorResponse(requestBuffer.toString(), metaInfo, ex).getContent();
+            Map<String,String> metaInfo, ServiceException ex) throws IOException {
+        return new Status(ex.getCode(), ex.getMessage()).getJson().toString(2);
     }
 
     /**
