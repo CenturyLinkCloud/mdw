8aecc93d04f3f5e9c300322b09a53cb65f5cd7a7
Manoj Agrawal <manoj.agrawal@centurylink.com>
2018-01-02 12:20:17 -0700

    Issue #284

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     cd mdw
-    gradle -DPUBLISHING_TO_MAVEN_CENTRAL=true
+    gradle -DPUBLISHING_TO_MAVEN_CENTRAL=true publishAssetsToMavenCentral
 fi
