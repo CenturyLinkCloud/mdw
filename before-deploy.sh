4243b9c23d03129b0837320e4b2242836bf76b33
Manoj Agrawal <manoj.agrawal@centurylink.com>
2018-01-02 12:10:48 -0700

    Issue #284

---
+++ before-deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+	openssl aes-256-cbc -K $encrypted_12077084ea60_key -iv $encrypted_12077084ea60_iv -in codesigning.asc.enc -out codesigning.asc -d
+    gpg --fast-import codesigning.asc
+fi
