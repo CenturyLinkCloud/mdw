336634d43f80cf28e4723d2d61e51cf8ab234cd9
Manoj Agrawal <manoj.agrawal@centurylink.com>
2018-01-02 17:33:38 -0700

    Issue #284

--- before-deploy.sh
+++ before-deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-if [ "${TRAVIS_BRANCH}" = "master" ];then
-    if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-        echo "Preparing Publishing assets..."
-        openssl aes-256-cbc -K $encrypted_12077084ea60_key -iv $encrypted_12077084ea60_iv -in codesigning.asc.enc -out codesigning.asc -d
-        gpg --fast-import codesigning.asc
-        chmod 0600 codesigning.asc
+if [ "${TRAVIS_BRANCH}" = "master" ] 
+then
+    if [ "${TRAVIS_PULL_REQUEST}" = "false" ] 
+    then
+          echo "Preparing Publishing assets..."
+          openssl aes-256-cbc -K $encrypted_12077084ea60_key -iv $encrypted_12077084ea60_iv -in codesigning.asc.enc -out codesigning.asc -d
+          gpg --fast-import codesigning.asc
+          chmod 0600 codesigning.asc
     fi
 fi
--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    if [ "${TRAVIS_PULL_REQUEST}" -eq "false" ]; then
-        cd mdw
-        pwd
-        echo "Publishing assets..."
-        gradle -DPUBLISHING_TO_MAVEN_CENTRAL=true publishAssetsToMavenCentral
+if [ "${TRAVIS_BRANCH}" = "master" ] 
+then
+    if [ "${TRAVIS_PULL_REQUEST}" -eq "false" ] 
+    then
+          cd mdw
+          pwd
+          echo "Publishing assets..."
+          gradle -DPUBLISHING_TO_MAVEN_CENTRAL=true publishAssetsToMavenCentral
     fi
 fi
