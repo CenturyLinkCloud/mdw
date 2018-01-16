8c4996b72c195fa45d1df955c8cbb37f145898a2
Donald Oakes <donald@oakesville.io>
2018-01-15 20:07:24 -0700

    tree view

--- mdw-workflow/assets/com/centurylink/mdw/system/filepanel/Index.jsx
+++ mdw-workflow/assets/com/centurylink/mdw/system/filepanel/Index.jsx
@@ -1,18 +1,49 @@
 import React, {Component} from '../../node/node_modules/react';
 import PropTypes from '../../node/node_modules/prop-types';
+import TreeView from '../../node/node_modules/react-treeview';
+import '../../node/node_modules/style-loader!../../react/react-treeview.css';
 
 class Index extends Component {
   constructor(...args) {
     super(...args);
+    this.state = { rootDirs: [] };
   }
   
   componentDidMount() {
+    fetch(new Request(this.getChildContext().serviceRoot + '/com/centurylink/mdw/system/filepanel', {
+      method: 'GET',
+      headers: { Accept: 'application/json'},
+      credentials: 'same-origin'
+    }))
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      this.setState({
+        rootDirs: json.dirs
+      });
+    });
   }
   
   render() {
     return (
       <div>
-        HELLO
+        {
+          this.state.rootDirs.map(dir => {
+            return (
+              <TreeView key={dir.path} nodeLabel={dir.path} defaultCollapsed={false}>
+                {
+                  dir.files.map(file => {
+                    const fileLabel = <span className="node">{file.path}</span>;
+                    return (
+                      <div key={file.path} className="info">{file.path}</div>
+                    );
+                  })
+                }
+              </TreeView>
+            );
+          })
+        }
       </div>
     );
   }
