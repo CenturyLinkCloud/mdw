891ce4383046dd9edf513e9873bdb0e9936e0f0a
Donald Oakes <donald@oakesville.io>
2018-01-19 17:26:56 -0700

    scrolling

--- mdw-workflow/assets/com/centurylink/mdw/system/filepanel/FileView.jsx
+++ mdw-workflow/assets/com/centurylink/mdw/system/filepanel/FileView.jsx
@@ -7,7 +7,19 @@ import '../../node/node_modules/style-loader!./filepanel.css';
 class FileView extends Component {
   constructor(...args) {
     super(...args);
-    this.state = {item: {}, buffer: {}, lineIndex: 0};
+    let optsStr = localStorage.getItem('filepanel-options');
+    if (optsStr) {
+      this.options = JSON.parse(optsStr);
+    }
+    else {
+      this.options = {};
+    }
+    // default options
+    this.options.bufferSize = FileView.BUFFER_SIZE;  // TODO: options
+    this.options.fetchThreshold = FileView.FETCH_THRESHOLD; // TODO: options
+    
+    this.state = {item: {}, buffer: {length: 0}, lineIndex: 0};
+    this.fetchLines = this.fetchLines.bind(this);
     this.handleScroll = this.handleScroll.bind(this);
     this.handleOptions = this.handleOptions.bind(this);
   }
@@ -18,38 +30,52 @@ class FileView extends Component {
   componentWillReceiveProps(props) {
     // retrieve fileView
     if (props.item.path) {
-      // clear while loading
       this.setState({
         item: props.item,
-        lines: ''
+        buffer: {length: 0},
+        lineIndex: 0
       });
-      
-      let url = this.context.serviceRoot + '/com/centurylink/mdw/system/filepanel';
-      url += '?path=' + encodeURIComponent(props.item.path);
-      url += '&lineIndex=' + this.state.lineIndex;
-      fetch(new Request(url, {
-        method: 'GET',
-        headers: { Accept: 'application/json'},
-        credentials: 'same-origin'
-      }))
-      .then(response => {
-        return response.json();
-      })
-      .then(json => {
-        this.setState({
-          item: json.info,
-          buffer: json.buffer
-        });
-        this.props.onInfo(json.info);
-      });
+      this.fetchLines(props);
     }
   }
   
+  fetchLines(props) {
+    let url = this.context.serviceRoot + '/com/centurylink/mdw/system/filepanel';
+    url += '?path=' + encodeURIComponent(props.item.path);
+    url += '&lineIndex=' + this.state.lineIndex;
+    if (this.options.bufferSize) {
+      url += '&bufferSize=' + this.options.bufferSize;
+    }
+    fetch(new Request(url, {
+      method: 'GET',
+      headers: { Accept: 'application/json'},
+      credentials: 'same-origin'
+    }))
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      this.setState({
+        item: json.info,
+        buffer: json.buffer,
+        lineIndex: this.state.lineIndex
+      });
+      this.props.onInfo(json.info);
+    });
+  }
+  
   handleScroll(values) {
+    let lineIndex = Math.round(values.scrollTop * this.state.buffer.length / values.scrollHeight);
+    this.setState({
+      item: this.state.item,
+      lines: this.state.lines,
+      lineIndex: lineIndex
+    });
     console.log("SCROLL: " + JSON.stringify(values, null, 2));
   }
   
   handleOptions(options) {
+    this.options = options;
     localStorage.setItem('filepanel-options', JSON.stringify(options));
     this.setState({
       item: this.state.item,
@@ -59,22 +85,16 @@ class FileView extends Component {
   }
   
   render() {
-    var bufferSize = 100;  // TODO: options
-    var refetchThreshold = 10; // TODO: options
-    
     var lineNumbers = null;
     if (this.state.buffer.length && this.state.item.isFile && !this.state.item.binary) {
-      var options = localStorage.getItem('filepanel-options');
-      if (options) {
-        options = JSON.parse(options);
-        if (options.lineNumbers) {
-          lineNumbers = '';
-          for (let i = this.state.lineIndex + 1; i < this.state.buffer.length + 1; i++) {
-            lineNumbers += i;
-            if (i < this.state.buffer.length)
-              lineNumbers += '\n';
-          }
+      if (this.options.lineNumbers) {
+        lineNumbers = '';
+        for (let i = this.state.buffer.start + 1; i < this.state.buffer.length + 1; i++) {
+          lineNumbers += i;
+          if (i < this.state.buffer.length)
+            lineNumbers += '\n';
         }
+//        console.log("LINE_NUMS: " + lineNumbers);
       }
     }
     
@@ -91,11 +111,11 @@ class FileView extends Component {
               onScrollFrame={this.handleScroll}>
               <div>
                 {lineNumbers &&
-                  <div className="fp-line-numbers">
+                  <div id="fp-line-numbers" className="fp-line-numbers">
                     {lineNumbers}
                   </div>
                 }
-                <div className="fp-file-content">
+                <div id="fp-file-content" className="fp-file-content">
                   {this.state.buffer.lines}
                 </div>
               </div>
@@ -111,5 +131,8 @@ FileView.contextTypes = {
   hubRoot: PropTypes.string,
   serviceRoot: PropTypes.string
 };
+
+FileView.BUFFER_SIZE = 1000;
+FileView.FETCH_THRESHOLD = 100;
 
 export default FileView; 
