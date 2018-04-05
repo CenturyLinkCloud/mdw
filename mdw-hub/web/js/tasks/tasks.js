c45c9833103289545cfd893399775a5c036b8d45
naveena-bellamkonda <naveena.bellamkonda@centurylink.com>
2018-04-05 20:39:55 +0530

    issue #363(Fix Task Templates nav link) (#374)

--- mdw-hub/web/js/tasks/tasks.js
+++ mdw-hub/web/js/tasks/tasks.js
@@ -242,6 +242,64 @@ tasksMod.controller('TasksController', ['$scope', '$window', '$http', '$location
   
 }]);
 
+tasksMod.controller('TemplatesController', ['$scope', '$cookieStore', 'mdw', 'util', 'Templates',
+										   function($scope, $cookieStore, mdw, util, Templates) {
+  $scope.templateList = Templates.retrieve({grouped:true}, function success() {
+	var pkgs = $scope.templateList.packages;
+	pkgs.forEach(function(pkg) {
+	  pkg.templates.forEach(function(template) {
+		template.baseName = template.name.substring(0, template.name.lastIndexOf('.'));
+	  });
+	  $scope.applyPkgCollapsedState();
+	});
+  });
+
+  $scope.collapse = function(pkg) {
+    pkg.collapsed = true;
+    $scope.savePkgCollapsedState();
+  };
+  $scope.collapseAll = function() {
+	$scope.templateList.packages.forEach(function(pkg) {
+	  pkg.collapsed = true;
+	});
+	$scope.savePkgCollapsedState();
+  };
+  $scope.expand = function(pkg) {
+	pkg.collapsed = false;
+	$scope.savePkgCollapsedState();
+  };
+  $scope.expandAll = function() {
+	$scope.templateList.packages.forEach(function(pkg) {
+	  pkg.collapsed = false;
+	});
+	$scope.savePkgCollapsedState();
+  };
+  $scope.savePkgCollapsedState = function() {
+    var st = {};
+    $scope.templateList.packages.forEach(function(pkg) {
+      if (pkg.collapsed)
+        st[pkg.name] = true;
+    });
+    $cookieStore.put('procsPkgCollapsedState', st);
+  };
+  $scope.applyPkgCollapsedState = function() {
+	var st = $cookieStore.get('procsPkgCollapsedState');
+	if (st) {
+	  util.getProperties(st).forEach(function(pkgName) {
+		var col = st[pkgName];
+		if (col === true) {
+		  for (var i = 0; i < $scope.templateList.packages.length; i++) {
+			if (pkgName == $scope.templateList.packages[i].name) {
+			  $scope.templateList.packages[i].collapsed = true;
+			  	break;
+			}
+		  }
+		}
+	  });
+	}
+  };
+}]);
+
 tasksMod.factory('TaskAction', ['$resource', 'mdw', function($resource, mdw) {
   return $resource(mdw.roots.services +'/Services/Tasks/:action', mdw.serviceParams(), {
     create: { method: 'POST'},
@@ -285,4 +343,10 @@ tasksMod.factory('TaskUtil', ['$location', '$route', 'mdw', 'util', function($lo
       }
     }
   };
+}]);
+
+tasksMod.factory('Templates', ['$resource', 'mdw', function($resource, mdw) {
+  return $resource(mdw.roots.services + '/Services/Tasks/templates', mdw.serviceParams(), {
+    retrieve: { method: 'GET', isArray: false }
+  });
 }]);
