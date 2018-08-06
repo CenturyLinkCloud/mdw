b4675b1ae67665825e9e956c2766c47341b4ebd6
Donald Oakes <donald.oakes@centurylink.com>
2018-08-06 17:26:03 -0600

    docs and templates

--- mdw/templates/{{spring-boot}}/src/main/java/com/example/MyApplication.java
+++ mdw/templates/{{spring-boot}}/src/main/java/com/example/MyApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-import com.centurylink.mdwdemo.DemoApplication;
-
 @SpringBootApplication
 @ServletComponentScan({"com.centurylink.mdw.boot.servlet","com.centurylink.mdw.hub.servlet"})
 public class MyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(MyApplication.class, args);
     }
 }
