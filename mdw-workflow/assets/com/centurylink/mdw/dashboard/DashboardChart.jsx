433bf4f08bd11e63e9857c8585acc7abbd9bbccd
Donald Oakes <donald.oakes@centurylink.com>
2019-02-26 11:03:34 -0700

    issue #624

--- mdw-workflow/assets/com/centurylink/mdw/dashboard/DashboardChart.jsx
+++ mdw-workflow/assets/com/centurylink/mdw/dashboard/DashboardChart.jsx
@@ -191,7 +191,7 @@ class DashboardChart extends Component {
       var downloadUrl = this.buildUrl(this.context.serviceRoot + breakdown.data);
       if (breakdown.instancesParam) {
         downloadUrl += '&' + breakdown.instancesParam + '=%5B' + this.state.selected.map(sel => {
-          return breakdown.instancesParam === 'statuses' ? sel.name : sel.id;
+          return breakdown.selectField ? encodeURI(sel[breakdown.selectField]) : encodeURI(sel.id);
         }).join() + '%5D';
       }
       location = downloadUrl + '&DownloadFormat=xlsx';
@@ -280,7 +280,7 @@ class DashboardChart extends Component {
         var dataUrl = this.buildUrl(this.context.serviceRoot + breakdown.data);
         if (breakdown.instancesParam) {
           dataUrl += '&' + breakdown.instancesParam + '=%5B' + this.state.selected.map(sel => {
-            return breakdown.selectField ? sel[breakdown.selectField] : sel.id;
+            return breakdown.selectField ? encodeURI(sel[breakdown.selectField]) : encodeURI(sel.id);
           }).join() + '%5D';
         }
         fetch(new Request(dataUrl, {
