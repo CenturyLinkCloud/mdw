632d04e34054928c3667b0406d08abeac81a749b
Donald Oakes <donald.oakes@centurylink.com>
2019-03-30 12:45:33 -0600

    Studio 71

--- mdw-common/src/com/centurylink/mdw/constant/WorkTransitionAttributeConstant.java
+++ mdw-common/src/com/centurylink/mdw/constant/WorkTransitionAttributeConstant.java
@@ -20,6 +20,5 @@ public class WorkTransitionAttributeConstant  {
      public static final String TRANSITION_DELAY = "TRANSITION_DELAY";
 
      public static final String TRANSITION_RETRY_COUNT = "TRANSITION_RETRY_COUNT";
-     public static final String TRANSITION_RETRY_DELAY = "TRANSITION_RETRY_DELAY";
      public static final String TRANSITION_DISPLAY_INFO = "TRANSITION_DISPLAY_INFO";
 }
