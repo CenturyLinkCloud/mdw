407f1f8cad729bd63ec089baf79c3188b7fcb9f5
Donald Oakes <donald.oakes@centurylink.com>
2019-04-17 12:47:57 -0600

    #646

--- mdw-workflow/assets/com/centurylink/mdw/dashboard/System.jsx
+++ mdw-workflow/assets/com/centurylink/mdw/dashboard/System.jsx
@@ -43,8 +43,8 @@ class System extends Component {
           name: 'Heap Memory',
           units: 'MB',
           summaryTitle: 'Rolling Average',
-          tops: '/System/metrics/Memory/summary',
-          data: '/System/metrics/Memory',
+          tops: '/System/metrics/HeapMemory/summary',
+          data: '/System/metrics/HeapMemory',
           websocketUrl: webSocketUrl,
           colors: ['#3366CC','#22AA99'],
           fill: true,
@@ -67,7 +67,7 @@ class System extends Component {
           data: '/System/metrics/ThreadPool',
           websocketUrl: webSocketUrl,
           colors: ['#3366CC','#DC3912'],
-          fill: [true,true,true],
+          fill: [true,false,false],
           chartOptions: { 
             animation: false,
             scales: {
@@ -75,6 +75,28 @@ class System extends Component {
                 ticks: {
                   beginAtZero: true,
                   max: undefined // must unset options from above
+                }
+              }]
+            }
+          }
+        },
+        {
+          name: 'DB Connections',
+          summaryTitle: 'Rolling Average',
+          tops: '/System/metrics/DbConnections/summary',
+          data: '/System/metrics/DbConnections',
+          websocketUrl: webSocketUrl,
+          colors: ['#3366CC','#22AA99'],
+          fill: [true,true,false],
+          stacked: [true,true,false],
+          chartOptions: { 
+            animation: false,
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true,
+                  max: undefined,
+                  callback: function(value) {if (value % 1 === 0) return value;}
                 }
               }]
             }
