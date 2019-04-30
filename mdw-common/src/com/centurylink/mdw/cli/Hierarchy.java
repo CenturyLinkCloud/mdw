686eaa17c6b16042b65a0cb73d5971d395b4e129
naveena-bellamkonda <naveena.bellamkonda@centurylink.com>
2019-04-30 17:16:02 +0530

    issue #651 process hierarchy

--- mdw-common/src/com/centurylink/mdw/cli/Hierarchy.java
+++ mdw-common/src/com/centurylink/mdw/cli/Hierarchy.java
@@ -1,24 +1,64 @@
 package com.centurylink.mdw.cli;
 
+import com.beust.jcommander.Parameter;
 import com.centurylink.mdw.model.workflow.Activity;
+import com.centurylink.mdw.model.workflow.LinkedProcess;
 import com.centurylink.mdw.model.workflow.Process;
 import com.centurylink.mdw.model.workflow.ProcessHierarchy;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class Hierarchy extends Setup {
+    @Parameter(names = "--process", description = "Process for which heirarchy will be shown.")
+    private String process;
+    public String getProcess() {
+        return process;
+    }
+    public void setProcess(String proc) {
+        this.process = proc;
+    }
+
+    private List<LinkedProcess> topLevelCallers;
+
+    private JSONObject processHierarchy;
+
+    public JSONObject getProcessHierarchy() {
+        return processHierarchy;
+    }
+
+    Hierarchy() {
+
+    }
+
+    Hierarchy (String process) {
+        this.process = process;
+    }
 
     @Override
     public Operation run(ProgressMonitor... monitors) throws IOException {
-        // TODO load process hierarchy
-
+        topLevelCallers = new ArrayList<LinkedProcess>();
+        int index = process.lastIndexOf('/');
+        String pkgName = process.substring(0, index);
+        String pkgFile = getAssetRoot() + "/" + pkgName.replace('.', '/') + "/";
+        String procName = process.substring(index + 1);
+        Process proc = new Process(new JSONObject(Files.readAllBytes(Paths.get(pkgFile + procName))));
+        proc.setName(procName.substring(0, procName.length()-5));
+        proc.setPackageName(pkgName);
+        Map<String,File> packageDirs = getAssetPackageDirs();
+        Map<String,Properties> versionProps = getVersionProps(packageDirs);
+        Properties pkgProps = versionProps.get(pkgName);
+        proc.setVersion(Integer.parseInt(pkgProps.getProperty(procName)));
+        addTopLevelCallers(proc);
+        for (LinkedProcess topLevelCaller : topLevelCallers) {
+            addCalledHierarchy(topLevelCaller);
+        }
+        processHierarchy = new JSONObject();
         return this;
     }
 
@@ -50,7 +90,7 @@ public class Hierarchy extends Setup {
         List<Process> processes = new ArrayList<>();
         for (String pkg : getPackageProcesses().keySet()) {
             for (File procFile : getPackageProcesses().get(pkg)) {
-                processes.add(loadProcess(pkg, procFile, false));
+                processes.add(loadProcess(pkg, procFile, true));
             }
         }
         return ProcessHierarchy.findInvoked(mainproc, processes);
@@ -87,5 +127,36 @@ public class Hierarchy extends Setup {
         if (packageVersions == null)
             packageVersions = getVersionProps(getAssetPackageDirs());
         return packageVersions;
+    }
+
+    private void addTopLevelCallers(Process called)
+            throws IOException {
+        List<Process> immediateCallers = findCallingProcesses(called);
+        if (immediateCallers.isEmpty()) {
+            topLevelCallers.add(new LinkedProcess(called));
+        }
+        else {
+            for (Process caller : immediateCallers)
+                addTopLevelCallers(caller);
+        }
+    }
+
+    private void addCalledHierarchy(LinkedProcess caller)
+            throws IOException {
+        Process callerProcess = caller.getProcess();
+        for (Process calledProcess : findCalledProcesses(callerProcess).toArray(new Process[0])) {
+            LinkedProcess child = new LinkedProcess(calledProcess);
+            child.setParent(caller);
+            caller.getChildren().add(child);
+            addCalledHierarchy(child);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Hierarchy hierarchy = new Hierarchy();
+        hierarchy.setConfigLoc("C:\\workspaces\\mdw6\\mdw\\config");
+        hierarchy.setAssetLoc("C:\\workspaces\\mdw6\\mdw-workflow\\assets");
+        hierarchy.setProcess("com.centurylink.mdw.tests.workflow/SmartProcessChild.proc");
+        hierarchy.run(null);
     }
 }
