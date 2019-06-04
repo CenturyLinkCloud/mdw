c8947611c4ff0a58d884608667749830f1b65015
Donald Oakes <donald.oakes@centurylink.com>
2019-06-04 11:29:43 -0600

    issue #674

--- mdw-workflow/assets/com/centurylink/mdw/tests/services/RootPath.java
+++ mdw-workflow/assets/com/centurylink/mdw/tests/services/RootPath.java
@@ -1,6 +1,5 @@
 package com.centurylink.mdw.tests.services;
 
-import com.centurylink.mdw.common.service.ServiceException;
 import com.centurylink.mdw.services.rest.JsonRestService;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -8,14 +7,11 @@ import org.json.JSONObject;
 import javax.ws.rs.Path;
 import java.util.Map;
 
-/**
- * TODO: changing below to "/" path breaks SwaggerValidatorActivity
- */
-@Path("/todo")
+@Path("/")
 public class RootPath extends JsonRestService {
 
     @Override
-    public JSONObject get(String path, Map<String,String> headers) throws ServiceException, JSONException {
+    public JSONObject get(String path, Map<String,String> headers) throws JSONException {
         JSONObject json = new JSONObject();
         json.put("impl", getClass().getName());
         return json;
