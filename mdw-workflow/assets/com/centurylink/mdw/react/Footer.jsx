4fa7ffaa760a354912aa8c95be1d2ed7723d95d7
Donald Oakes <donald.oakes@centurylink.com>
2020-02-18 15:35:49 -0700

    #803

--- mdw-workflow/assets/com/centurylink/mdw/react/Footer.jsx
+++ mdw-workflow/assets/com/centurylink/mdw/react/Footer.jsx
@@ -5,9 +5,18 @@ function Footer(props) { // eslint-disable-line no-unused-vars
   return (
     <div id="mdw-footer">
       <div className="mdw_footer">
+        {$mdwAppId && $mdwAppId !== 'Unknown' && $mdwAppId !== 'mdw6' &&
+          <span>
+            {$mdwAppId}
+            {$mdwAppVersion && $mdwAppVersion != 'Unknown' &&
+              <span> {$mdwAppVersion}</span>
+            }
+            <br/>
+          </span>
+        }
         MDW {$mdwVersion}
         <br/>
-        Copyright &#169; 2019 CenturyLink, Inc.
+        Copyright &#169; 2020 CenturyLink, Inc.
       </div>
     </div>
   );
